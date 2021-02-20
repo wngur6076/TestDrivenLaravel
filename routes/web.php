652e78d1157f34b6153f292d8a975319ed4b8a8a
wngur6076 <wngur6076@naver.com>
2021-02-20 17:15:47 +0900

    Book Checkout & Book Checkin Flow Feature Test With TDD - Part 2

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\CheckinBookController;
+use App\Http\Controllers\CheckoutBookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,10 @@ Route::patch('/books/{book}', [BooksController::class, 'update']);
 Route::delete('/books/{book}', [BooksController::class, 'destroy']);
 
 Route::post('author', [AuthorsController::class, 'store']);
+
+Route::post('/checkout/{book}', [CheckoutBookController::class, 'store']);
+Route::post('/checkin/{book}', [CheckinBookController::class, 'store']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
